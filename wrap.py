778a95cc17ff72b91fe26844ae9b67e6f26ca733
Pebaz <pebazium@gmail.com>
2019-12-02 16:03:33 -0500

    Fix typo

--- wrap.py
+++ wrap.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        print('col-wrap: Wrap long columns in a wide terminal.')
+        print('wrap: Wrap long columns in a wide terminal.')
         sys.exit(0)
 
     col = int(sys.argv[1])
