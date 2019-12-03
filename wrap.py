0ee73b9e05ba8d221fec24eb4523a06ff2f24552
Wilder <wildsamu@a483e7012ecc.ant.amazon.com>
2019-12-03 11:49:17 -0500

    Added Rust Implementation

--- wrap.py
+++ wrap.py
@@ -2,7 +2,8 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        print('wrap: Wrap long columns in a wide terminal.')
+        println!('Wrap long columns in a wide terminal.');
+        println!('Usage: wrap <column>\nExample: ls -lah | wrap 60')
         sys.exit(0)
 
     col = int(sys.argv[1])
